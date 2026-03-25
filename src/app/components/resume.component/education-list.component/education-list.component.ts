02c4e662de30d222dabca2eebac7fd81cf1b320e
Adam Hilty <hilty.89@osu.edu>
2026-03-25 14:24:06 -0400

    Implemented education list component

---
+++ src/app/components/resume.component/education-list.component/education-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { EducationProgram } from '../../../models/education-list.model';
+
+/**
+ * Education List Component
+ * A presentational component that displays a formatted list of academic degrees and certifications.
+ * @remarks
+ * It receives its data strictly via Property Binding from the {@link ResumeComponent}.
+ */
+@Component({
+  selector: 'education-list',
+  standalone: true,
+  templateUrl: './education-list.component.html',
+  styleUrl: './education-list.component.scss',
+})
+export class EducationListComponent {
+  /** 
+   * The collection of educational programs to be rendered.
+   * This is a required input passed down from the Resume container.
+   */
+  @Input({ required: true }) educationList: EducationProgram[] = [];
+}
