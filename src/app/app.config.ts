b83d53661c084057540305cd89c600bf2ccf6904
Adam Hilty <hilty.89@osu.edu>
2026-03-25 14:00:06 -0400

    Configured routes and scrolling behavior

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
+/**
+ * Global Application Configuration for the Angular Portfolio.
+ * This object defines the providers and environmental configurations that are 
+ * initialized when the application bootstraps. 
+ */
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
-};
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter( routes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled'}) ), provideHttpClient()]
+}
